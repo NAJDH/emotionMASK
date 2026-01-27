28fd0a2b0a08b0ee04a2c74d0bc5c257b2f3111a
comeing <109682810+NAJDH@users.noreply.github.com>
2026-01-27 10:18:40 +0800

    111

--- emotionMASK/Assets/c#/enemy/Enemy.cs
+++ emotionMASK/Assets/c#/enemy/Enemy.cs
@@ -4,6 +4,43 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int EntityDirection{get; private set;} = 1;
+    private bool isFacingRight = true;
+    public Animator anim{get; private set;}
+    public Rigidbody2D rb{get; private set;}
+    protected EnemyStateMachine stateMachine;
+    protected void Awake()
+    {
+        stateMachine = new EnemyStateMachine();
+    }
+    protected void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    protected void Update()
+    {
+        stateMachine.currentState.Update();
+    }
+    #region 翻转函数
+    public void Flip()
+    {
+        EntityDirection *= -1;
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+    public void FilpController(float x)
+    {
+        if(x > 0 && !isFacingRight) Flip();
+        else if(x < 0 && isFacingRight) Flip();
+    }
+    #endregion
+    public void SetZeroVelocity()
+    {
+        // if(isknockback) return;//受击击退的时候不会有其他速度
+
+        rb.velocity = new Vector2(0f, rb.velocity.y);
+    }
     public void Damage()
     {
         Debug.Log("Enemy Hit!");
---
+++ emotionMASK/Assets/c#/enemy/EnemyState.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState
+{
+    // protected EnemyAnimationEvent EnemyAnimationEvent => enemybase.GetComponentInChildren<EnemyAnimationEvent>();
+    protected EnemyStateMachine stateMachine;
+    protected Enemy enemybase;
+    protected string animBoolName;
+    protected float stateTimer;
+    public EnemyState(Enemy enemybase, EnemyStateMachine stateMachine, string animBoolName)
+    {
+        this.enemybase = enemybase;                           
+        this.stateMachine = stateMachine;
+        this.animBoolName = animBoolName;
+    }
+    #region 状态机的三个核心函数
+    public virtual void Enter()                       //进入状态
+    {
+        enemybase.anim.SetBool(animBoolName, true);
+    }
+    public virtual void Update()                      //更新状态
+    {
+        stateTimer -= Time.deltaTime;
+    }
+    public virtual void Exit()                        //退出状态
+    {
+        enemybase.anim.SetBool(animBoolName, false);
+    }
+    #endregion
+}
